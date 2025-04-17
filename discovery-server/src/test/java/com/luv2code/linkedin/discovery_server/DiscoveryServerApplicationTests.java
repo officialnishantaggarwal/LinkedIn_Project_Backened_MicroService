77f7b3b68f9e6665b8cae03b5adc2465860b3f5e
nishantaggarwal <naggarwal@mafcarrefour.com>
2025-04-17 22:21:34 +0530

    connection mircroservice and api gateway

---
+++ discovery-server/src/test/java/com/luv2code/linkedin/discovery_server/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.linkedin.discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
